7bde28ae02df70d752dbc6fc6b08fd638cd11419
Santy <pizarro.santy@gmail.com>
2023-05-10 11:10:16 -0300

    metodos para empleado

--- src/ar/edu/unlam/pb/Empleado.java
+++ src/ar/edu/unlam/pb/Empleado.java
@@ -1,5 +1,81 @@
 package ar.edu.unlam.pb;
 
+import java.util.Objects;
+
 public class Empleado {
+	private String Nombre;
+	private String Dni;
+	private Integer Sueldo;
+	private Integer cantidadDeFaltas;
+	private Integer cantidadDeAdvertencias;
+	
+	public Empleado(String Nombre, String Dni, Integer Sueldo) {
+			this.Dni=Dni;
+			this.Nombre=Nombre;
+			this.Sueldo=Sueldo;
+			cantidadDeFaltas=0;
+			cantidadDeAdvertencias=0;		
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(Dni);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		return Objects.equals(Dni, other.Dni);
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	public String getDni() {
+		return Dni;
+	}
+	
+	public void setDni(String dni) {
+		Dni = dni;
+	}
+	
+	public Integer getSueldo() {
+		return Sueldo;
+	}
+
+	public void setSueldo(Integer sueldo) {
+		Sueldo = sueldo;
+	}
+
+	
+	public int getCantidadDeFaltas() {
+		return cantidadDeFaltas;
+	}
+	
+	public void setCantidadDeFaltas(Integer cantidadDeFaltas) {
+		this.cantidadDeFaltas = cantidadDeFaltas;
+	}
+	
+	public int getCantidadDeAdvertencias() {
+		return cantidadDeAdvertencias;
+	}
+	
+
+	public void setCantidadDeAdvertencias(Integer cantidadDeAdvertencias) {
+		this.cantidadDeAdvertencias = cantidadDeAdvertencias;
+	}
 
 }
