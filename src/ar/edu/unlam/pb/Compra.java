f5fa7d87bff3531b8d4feda09ad8f0ce17b50e39
TomasPezzano <pezzanotomas@gmail.com>
2023-05-10 11:13:11 -0300

    realizando metodos

--- src/ar/edu/unlam/pb/Compra.java
+++ src/ar/edu/unlam/pb/Compra.java
@@ -1,5 +1,59 @@
 package ar.edu.unlam.pb;
 
+import java.util.ArrayList;
+
+import supermercado.dominio.Miembro;
+import supermercado.dominio.Producto;
+
+
 public class Compra {
 
+	private Caja caja;
+	private Empleado empleado;
+	private Miembro miembro;
+	private ArrayList<Producto> productos;
+	
+	
+	
+	public Compra(Caja caja, Empleado empleado, Miembro miembro) {
+	
+		this.caja = caja;
+		this.empleado = empleado;
+		this.miembro = miembro;
+		this.productos = new ArrayList<Producto>();
+	}
+
+
+
+	public Compra() {
+		
+	}
+	
+	public void agregarProducto(Producto producto) {
+		productos.add(producto);
+	}
+	
+	
+	public Double darUnVuelto(Miembro miembro,Caja caja,Producto producto) {
+		Double vuelto=0.0;
+		if(miembro.getSaldo()>producto.getPrecio()) {
+			vuelto=miembro.getSaldo()-producto.getPrecio();
+			Double dinero= caja.getDinero()+(miembro.getSaldo()-vuelto);
+			caja.setDinero(dinero);
+			miembro.setSaldo(vuelto);
+		}
+		return vuelto;
+	}
+	
+	public Double queSiLaCompraLaRealizaUnMiembroSeHagaUnDescuentoDe20Porciento(Miembro miembro,Producto producto){
+		Double precioFinal=0.0;
+		Double descuentoInicial=20.0;
+		if(miembro.getTARJETA_MIEMBRO()) {
+			Double descuento=descuentoInicial% producto.getPrecio();
+			 precioFinal= producto.getPrecio()-descuento;
+		}
+		return precioFinal;
+	}
+	
+	
 }
--- src/ar/edu/unlam/pb/MiembroBasico.java
+++ src/ar/edu/unlam/pb/MiembroBasico.java
@@ -2,7 +2,7 @@ package ar.edu.unlam.pb;
 
 public class MiembroBasico extends Miembro {
 
-	public MiembroBasico() {
-		
+	public MiembroBasico(String nombre, String fecha, int dni, Double saldo) {
+		super(nombre,fecha,dni,saldo);
 	}
 }
