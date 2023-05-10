2f79e3ddd726e3b7210f1fb04a402ced9a678411
TomasPezzano <pezzanotomas@gmail.com>
2023-05-10 09:52:54 -0300

    iniciando proyecto

---
+++ prueba/ar/edu/unlam/pb/Prueba.java
@@ -0,0 +1,17 @@
+package ar.edu.unlam.pb;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class Prueba {
+	
+	@Test
+	public void queSeCreeElSupermercado() {
+		Supermercado supermercado= new Supermercado();
+		assertNotNull(supermercado);
+	}
+
+}
