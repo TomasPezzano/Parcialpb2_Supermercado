7d9e5c96261089a9058a24a340fe9ef6f0a0eb06
TomasPezzano <pezzanotomas@gmail.com>
2023-05-10 10:47:26 -0300

    agregando atributos a clases

--- src/ar/edu/unlam/pb/Producto.java
+++ src/ar/edu/unlam/pb/Producto.java
@@ -2,7 +2,24 @@ package ar.edu.unlam.pb;
 
 public class Producto {
 
+	private Double precio;
+	
 	public Producto() {
 		
 	}
-}
+		public Producto(Double precio) {
+			this.precio=precio;
+		}
+	
+
+		public Double getPrecio() {
+			return precio;
+		}
+
+
+		public void setPrecio(Double precio) {
+			this.precio = precio;
+		}
+		
+	}
+
